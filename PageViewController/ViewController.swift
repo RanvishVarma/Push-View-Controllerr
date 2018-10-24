e03efba3d47a1377bf86931b83b8a1a512a61d45
Allvy <allvy@allvys-macbook-pro-2.local>
2018-10-24 12:17:51 +0530

    Initial Commit

---
+++ PageViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageViewController
+//
+//  Created by Allvy on 24/10/18.
+//  Copyright © 2018 Allvy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
